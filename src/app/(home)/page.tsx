422f29cf8fb706c078ee26d0fd224c430bbb960d
GerardoGtz95 <gerardo.gutierrez@caliente.mx>
2025-02-27 10:40:46 -0600

    SEO Improvements

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
 import { MainProducts } from 'app/componentes/home/MainProducts';
+
+export const metadata: Metadata = {
+  title: '🚀 Future Wolrd',
+  description: 'Welcome to the future world, a store from another century',
+  keywords: ['ecommerce', 'technology'],
+};
 
 export default function Home() {
   return (
